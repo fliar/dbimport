bf4ab14c784e60470fb7ae56a715be140e44f1fa
unknown <lig@TRGAME-LIG.uc888.cn>
2015-10-22 11:08:50 +0800

    parsing params

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 import(
 	"fmt"
+	"flag"
 )
 
 func main(){
-	fmt.Println("test run")
+	fmt.Println("mysql import")
+	folder := flag.String("f", "./", "folder holding files to import")
+	dbinfo := flag.String("d", "", "MySQL db connection info")
+	flag.Parse()
+	fmt.Println("input folder is ", *folder)
+	fmt.Println("connecting to ", *dbinfo)
 }
